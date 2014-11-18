d0d6217fa3b7afb03f91c49e9ee19e8b78f87d5e
kongdeyu <hustyiyi@gmail.com>
2014-11-18 19:35:12 +0800

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,30 +1,33 @@
 
 #include <iostream>
 
-template <typename InputIterator>
+template <typename InputIterator, typename T>
 bool binary_search(InputIterator begin,
         InputIterator end,
-        typename std::iterator_traits<InputIterator>::value_type element)
+        const T &element,
+        InputIterator &res)
 {
     if(begin >= end)
         return false;
     InputIterator middle = begin + (end - begin) / 2;
     if(element == *middle)
     {
+        res = middle;
         return true;
     }
     if(element < *middle)
     {
-        return binary_search(begin, middle, element);
+        return binary_search(begin, middle, element, res);
     }
-    return binary_search(middle + 1, end, element);
+    return binary_search(middle + 1, end, element, res);
 }
 
 int32_t main()
 {
     const int32_t size = 10;
     int32_t array[size] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::cout << binary_search(array, array + size, 50) << std::endl;
-    std::cout << binary_search(array, array + size, 30) << std::endl;
+    int32_t *p = NULL;
+    std::cout << binary_search(array, array + size, 5, p) << std::endl;
+    std::cout << binary_search(array, array + size, 30, p) << std::endl;
     return 0;
 }
