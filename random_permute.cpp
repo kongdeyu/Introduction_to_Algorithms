865ff9a3f41efc3171d4cd657e9bca75386b74d9
kongdeyu <hustyiyi@gmail.com>
2014-12-26 15:43:26 +0800

    Create random_permute.cpp

---
+++ random_permute.cpp
@@ -0,0 +1,84 @@
+
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+template <typename InputIterator>
+void print(InputIterator begin, InputIterator end)
+{
+    for(InputIterator it = begin; it != end; it++)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+}
+
+template <typename InputIterator>
+void permute_by_sorting(InputIterator begin, InputIterator end)
+{
+    ptrdiff_t size = end - begin;
+    int32_t model = static_cast<int32_t>(pow(size, 3));
+    std::vector<int32_t> keys;
+    for(InputIterator it = begin; it != end; it++)
+    {
+        keys.push_back(rand() % model);
+    }
+
+    std::vector<bool> symbol(model, false);
+    std::vector<int32_t> temp(model, 0);
+    InputIterator it_in = begin;
+    for(std::vector<int32_t>::iterator it = keys.begin();
+            it != keys.end();
+            it++)
+    {
+        temp.at(*it) = *it_in++;
+        symbol.at(*it) = true;
+    }
+
+    size_t index = 0;
+    it_in = begin;
+    for(std::vector<int32_t>::iterator it = temp.begin();
+            it != temp.end();
+            it++)
+    {
+        if(symbol.at(index++))
+            *it_in++ = *it;
+    }
+}
+
+template <typename InputIterator>
+void permute_in_place(InputIterator begin, InputIterator end)
+{
+    ptrdiff_t size = end - begin;
+    size_t index = 0;
+    for(InputIterator it = begin; it != end; it++)
+    {
+        ptrdiff_t temp = rand() % (size - index);
+        std::swap(*it, *(it + temp));
+        index++;
+    }
+}
+
+int32_t main()
+{
+    std::vector<int32_t> array;
+    const int32_t size = 10;
+    for(int32_t i = 0; i < size; i++)
+    {
+        array.push_back(i);
+    }
+    std::cout << "permute by sortint:" << std::endl;
+    permute_by_sorting(array.begin(), array.end());
+    print(array.begin(), array.end());
+
+    array.clear();
+    for(int32_t i = 0; i < size; i++)
+    {
+        array.push_back(i);
+    }
+    std::cout << "permute in place:" << std::endl;
+    permute_in_place(array.begin(), array.end());
+    print(array.begin(), array.end());
+    return 0;
+}
